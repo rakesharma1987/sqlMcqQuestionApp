24567ab4ec0b5ab8a663fe868e4a0de89865ea5a
rakeshsharma <kumarakesharma@gmail.com>
2024-11-18 22:52:33 +0530

    db created

---
+++ app/src/main/java/com/example/sqlmcqapplication/db/AppDatabase.kt
@@ -0,0 +1,27 @@
+package com.example.sqlmcqapplication.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [QuestionEntity::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+    companion object{
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase{
+            var instance = INSTANCE
+            synchronized(this){
+                if (instance == null){
+                    instance = Room.databaseBuilder(context, AppDatabase::class.java, "Question")
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+            }
+            return instance!!
+        }
+    }
+}
